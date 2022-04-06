9ac4fd66349d94a078b93310803f43e31a7b4acd
ishola azeez <isholaazeezayo@gmail.com>
2022-04-06 20:35:24 +0100

    sure

--- contracts/AzNFT.sol
+++ contracts/AzNFT.sol
@@ -2,7 +2,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract AzNFT is ERC721URIStorage {
     uint public tokenCount;
-    constructor() ERC721("AzNFT", "AZNFT"){}
+    constructor() ERC721("AzNFT", "AZNFT"){
+
+    }
     function mint(string memory _tokenURI) external returns(uint) {
         tokenCount++;
         _selfMint(msg.sender, tokenCount);
